60df10913780c3e9a5af0d2bae16f647ff509ae3
Pogos <56889876+Pogosito@users.noreply.github.com>
2022-12-17 12:58:06 +0300

    Поправил код прогонки, обновил результаты

--- Baydoun_2018/main.cpp
+++ Baydoun_2018/main.cpp
@@ -7,11 +7,11 @@
 
 #include <iostream>
 #include <vector>
-#include "CubicPolynomial.hpp"
 #include "CubicPolynomialFMA.hpp"
 #include "excerpt.h"
+#define MAX_DISTANCE 1e-5
 
-#define MAX_DISTANCE 10e-5
+using namespace  std;
 
 /*
 
@@ -25,67 +25,11 @@
 
 */
 
-void smallTest() {
-
-	//	Exmp: 1
-	//	coief: 0 -0.0160999
-	//	coief: 1 0.19128
-	//	coief: 2 -0.757523
-	//	coief: 3 1
-
-	//	Exmp: 2
-	//	coief: 0 -0.0124015
-	//	coief: 1 0.160732
-	//	coief: 2 -0.694404
-	//	coief: 3 1
-
-	//	Exmp: 3
-	//	coief: 0 0.473331
-	//	coief: 1 1.82207
-	//	coief: 2 2.33799
-	//	coief: 3 1
-
-	// Exmp: 4
-	//	coief: 0 0.263884
-	//	coief: 1 1.23423
-	//	coief: 2 1.92424
-	//	coief: 3 1
-
-	std::vector<std::vector<long double>> coefficients = {
-		{-0.0160999, 0.19128, -0.757523},
-		{-0.694404, 0.160732, -0.0124015},
-		{ 2.33799,  1.82207, 0.473331 },
-		{1.92424 , 1.23423, 0.263884},
-	};
-
-	for (int i = 0; i < coefficients.size(); ++i) {
-		std::cout << std::endl;
-		std::cout << " -----------------" << std::endl;
-		std::cout << "Example # " << i << std::endl;
-		std::cout << " -----------------" << std::endl;
-		
-		std::complex<long double> complexnumber(coefficients[i][0], 0.0);
-		std::complex<long double> complexnumber2(coefficients[i][1], 0.0);
-		std::complex<long double> complexnumber3(coefficients[i][2], 0.0);
-
-		CubicPolynomial complexPolynom = CubicPolynomial(complexnumber, complexnumber2, complexnumber3);
-
-		std::vector<std::complex<long double>> arr = complexPolynom.calculateRoots();
-		for (int i = 0; i < arr.size(); ++i) {
-			std::cout << "root " << arr[i] << std::endl;
-		}
-
-		std::cout << " -----------------" << std::endl;
-
-		CubicPolynomialFMA <long double>helper(coefficients[i][0], coefficients[i][1], coefficients[i][2]);
-
-		std::vector<std::complex<long double>> arrFMA = helper.calculateRoots();
-		for (int i = 0; i < arrFMA.size(); ++i) {
-			std::cout << "root fma " << arrFMA[i] << std::endl;
-		}
-
-		std::cout << " -----------------" << std::endl;
-	}
+// Flexible suppression imaginary part of complex number
+template<typename fp_t>
+inline complex<fp_t> epsilonComplex(const complex<fp_t> &x)
+{
+	return abs(x) * numeric_limits<fp_t>::epsilon() > abs(x.imag()) ? complex<fp_t>(x.real(), 0) : x;
 }
 
 // Function to test cubic solution:
@@ -105,20 +49,33 @@ void testCubicAdv(const int testCount, const fp_t dist){
 
 	std::vector<fp_t> coefficients(P+1);
 	std::vector<fp_t> trueRoots(P);
+
+
 	for(int i=0; i < testCount; ++i){
+		std::vector<fp_t> realRoots;
+
 		generate_polynomial<fp_t>(P, 0, P, 0, dist,
 								  low, high, trueRoots, coefficients);
 
 		CubicPolynomialFMA <fp_t>helper(coefficients[2], coefficients[1], coefficients[0]);
 		std::vector<std::complex<fp_t>> myRoots = helper.calculateRoots();
 
-		std::vector<fp_t> imags = { abs(myRoots[0].imag()), abs(myRoots[1].imag()), abs(myRoots[2].imag()) };
-		int indexOfRealRoot = std::distance(std::begin(imags), std::min_element(std::begin(imags), std::end(imags)));
+		std::complex <fp_t> r1 = epsilonComplex(myRoots[0]);
+		std::complex <fp_t> r2 = epsilonComplex(myRoots[1]);
+		std::complex <fp_t> r3 = epsilonComplex(myRoots[2]);
 
-		std::vector<std::complex<fp_t>> helperRoots = { std::complex<fp_t>(myRoots[indexOfRealRoot].real(), 0) };
+		if(!r1.imag()){
+			realRoots.push_back(r1.real());
+		}
+		if(!r2.imag()){
+			realRoots.push_back(r2.real());
+		}
+		if(!r3.imag()){
+			realRoots.push_back(r3.real());
+		}
 
-		compare_roots_complex(3, P, helperRoots, trueRoots, absMaxError, relMaxError);
-
+		compare_roots<fp_t>(realRoots.size(), 3, realRoots,
+							trueRoots, absMaxError, relMaxError);
 		if(isinf(absMaxError))
 			cantFind += 1;
 		else{
@@ -140,61 +97,7 @@ void testCubicAdv(const int testCount, const fp_t dist){
 	std::cout<< "----------------------------------------------------" << std::endl;
 }
 
-template<typename fp_t>
-std::vector<fp_t> testPolynomial(unsigned int roots_count) {
-	fp_t max_absolute_error, max_relative_error;
-	std::vector<fp_t> roots(roots_count), coefficients(roots_count + 1);
-	generate_polynomial<fp_t>(roots_count, 0, roots_count, 0, MAX_DISTANCE, -1, 1, roots, coefficients);
-
-	CubicPolynomialFMA <fp_t>helper(coefficients[2], coefficients[1], coefficients[0]);
-	std::vector<std::complex<fp_t>> myRoots = helper.calculateRoots();
-
-	compare_roots_complex<fp_t>(coefficients.size(),
-								roots.size(), myRoots,
-								roots, max_absolute_error,
-								max_relative_error);
-
-//	std::cout << " -----------------" << std::endl;
-//
-//	std::cout << "coefficients" << std::endl;
-//	for (int i = 0; i < 3; ++i) {
-//		std::cout << coefficients[i] << std::endl;
-//	}
-//
-//	std::cout << "myRoots" << std::endl;
-//	for (int i = 0; i < 3; ++i) {
-//		std::cout << myRoots[i] << std::endl;
-//	}
-//
-//	std::cout << "roots" << std::endl;
-//	for (int i = 0; i < 3; ++i) {
-//		std::cout << roots[i] << std::endl;
-//	}
-//
-//	std::cout << " -----------------" << std::endl;
-
-	std::vector<fp_t> result = { max_absolute_error, max_relative_error };
-
-	return result;
-}
-
 int main(int argc, const char * argv[]) {
-
-	std::vector<float> absoluteErrors = {};
-	std::vector<float> relativeErrors = {};
-
-	for (int i = 0; i < 10'000'000; ++i) {
-		std::vector<float> errors = testPolynomial<float>(3);
-		absoluteErrors.push_back(errors[0]);
-		relativeErrors.push_back(errors[1]);
-	}
-//
-	std::vector<float>::iterator it = std::max_element(std::begin(absoluteErrors), std::end(absoluteErrors));
-	std::cout << "max absolute error " << (*it) << std::endl;
-
-	std::vector<float>::iterator it2 = std::max_element(std::begin(relativeErrors), std::end(relativeErrors));
-	std::cout << "max relative error " << (*it2) << std::endl;
-
-//	smallTest();
+	testCubicAdv<long double>(1000'000, 1e-5);
 	return 0;
 }
--- README.md
+++ README.md
@@ -5,13 +5,24 @@
 1. Полный повтор статьи
 2. С использованием функции fma для более точных вычислений
 
-# Эксперимент
+# Эксперименты
 
-Max distance: 0.0001
-Total count of tests: 1000000
+- Max_dist: 1e-05 
+- Test_Count: 1e-06 
+- Cant_find: 0 times
 
-Mean_Abs: 0.00100807
-Max_Abs: 0.00593473
-Mean_Rel: 0.00262643
-Max_Rel: 2
+## float
 
+- Mean_Abs: 0.000693906 
+- Max_Abs: 0.0374629 
+
+- Mean_Rel: 0.0014141 
+- Max_Rel: 0.185286 
+
+## double
+
+- Mean_Abs: 5.13373e-06 
+- Max_Abs: 1.66363e-05 
+ 
+- Mean_Rel: 2.5515e-05 
+- Max_Rel: 0.00941666 
